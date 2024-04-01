06b91e17c712cb6c969cc85d27ff644b0c4e1b67
juaancasillaa <juanacasilla16@outlook.com>
2024-04-01 10:33:23 -0400

    adding google proyect

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertSearch() {
+
+    var gooGles = document.getElementById( "googleinput" ).value;
+   
+    alert("You searched for: " + gooGles);
+    
+  }
+  
+  document.getElementById("ggsearch").addEventListener("click", alertSearch);
